bcdeee5a5313de1416d5829ca5517c71eee3f580
Abdul Razzaq <abdulrazzaqchohan1@gmail.com>
2024-01-17 18:06:29 +0500

    add a weather application

--- weather_project/weather/urls.py
+++ weather_project/weather/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.get_weather,name='get_weather'),
-    path('weather/',views.country_view,name='country'),
-    path('weather_country/',views.country_city_selector,name='country'),
+    # path('',views.get_weather,name='get_weather'),
+    path('',views.country_view,name='country'),
     # path('weather/',views.city_view,name='city'),
     path('weather/',views.get_weather,name='get_weather'),
     # path('weather/',views.get_cities,name='get_cities')
